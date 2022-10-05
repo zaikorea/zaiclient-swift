8457aded99a9efd49ab75768c557ccee64c3e9e9
kyungwon-lee <kw375310@gmail.com>
2022-10-05 15:51:56 +0900

    support carthage

---
+++ Sources/zaiclient/zaiclient.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for zaiclient.
+FOUNDATION_EXPORT double zaiclientVersionNumber;
+
+//! Project version string for zaiclient.
+FOUNDATION_EXPORT const unsigned char zaiclientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zaiclient/PublicHeader.h>
+
+
