ab426ed10281461d3584e4a642b29720ed8e191b
kyungwon-lee <kw375310@gmail.com>
2022-10-23 14:16:32 +0900

    edit recommendation test code

--- Example/Tests/TestUtils.swift
+++ Example/Tests/TestUtils.swift
@@ -9,10 +9,6 @@ import AWSCore
 let clientId: String = "test"
 let clientSecret: String = "KVPzvdHTPWnt0xaEGc2ix-eqPXFCdEV5zcqolBr_h1k"
 
-let zaiClient = ZaiClient(zaiClientID: clientId, zaiSecret: clientSecret)
-let incorrectIdClient = ZaiClient(zaiClientID: ".\(clientId)", zaiSecret: clientSecret)
-let incorrectSecretClient = ZaiClient(zaiClientID: clientId, zaiSecret: ".\(clientSecret)")
-
 let awsCredentials = AWSStaticCredentialsProvider(accessKey: Bundle.main.object(forInfoDictionaryKey: "ACCESS_KEY") as! String, secretKey: Bundle.main.object(forInfoDictionaryKey: "SECRET_KEY") as! String)
 let dynamodb = AWSDynamoDB.default()
 
@@ -61,62 +57,69 @@ public func checkSuccessfulEventAdd(event: BaseEvent?, completionHandler: @escap
     condition?.attributeValueList = [keyVal!]
     let expression = ["user_id": condition!]
     queryInput?.keyConditions = expression
+    
+    do {
+        let zaiClient = try ZaiClient(zaiClientID: clientId, zaiSecret: clientSecret)
 
-    zaiClient.addEventLog(event!) {
-        (res, error) in if let response = res {
-            
-            expect(response.failureCount).to(equal(0))
-            
-            if events?.count == 1 {
-                let e = events![0]
-                dynamodb.query(queryInput!) {
-                    (ddbRes, ddbError) in if let ddbResponse = ddbRes {
-                        expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
-                        expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
-
-                        let deleteInput = AWSDynamoDBDeleteItemInput()
-                        deleteInput?.tableName = "events_\(clientId)"
-                        deleteInput?.key = ["user_id": keyVal!, "timestamp": ddbResponse.items![0]["timestamp"]!]
-                        dynamodb.deleteItem(deleteInput!) {
-                            (res, err) in if let error = err {
-                                print(error.localizedDescription)
-                                fail()
-                            }
-                        }
-                        completionHandler(true, nil)
-                    } else {
-                        fail()
-                    }
-                }
+        zaiClient.addEventLog(event!) {
+            (res, error) in if let response = res {
                 
-            } else {
-                dynamodb.query(queryInput!) {
-                    (ddbRes, ddbError) in if let ddbResponse = ddbRes {
-                        expect(ddbResponse.items?.count).to(equal(events?.count))
-                        
-                        for res in ddbResponse.items! {
-                            expect(res["event_type"]?.s).to(equal(events![0].eventType))
-                            
+                expect(response.failureCount).to(equal(0))
+                
+                if events?.count == 1 {
+                    let e = events![0]
+                    dynamodb.query(queryInput!) {
+                        (ddbRes, ddbError) in if let ddbResponse = ddbRes {
+                            expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
+                            expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
+
                             let deleteInput = AWSDynamoDBDeleteItemInput()
                             deleteInput?.tableName = "events_\(clientId)"
-                            deleteInput?.key = ["user_id": keyVal!, "timestamp": res["timestamp"]!]
+                            deleteInput?.key = ["user_id": keyVal!, "timestamp": ddbResponse.items![0]["timestamp"]!]
                             dynamodb.deleteItem(deleteInput!) {
                                 (res, err) in if let error = err {
                                     print(error.localizedDescription)
                                     fail()
                                 }
                             }
-                            
                             completionHandler(true, nil)
+                        } else {
+                            fail()
                         }
-                    } else {
-                        fail()
+                    }
+                    
+                } else {
+                    dynamodb.query(queryInput!) {
+                        (ddbRes, ddbError) in if let ddbResponse = ddbRes {
+                            expect(ddbResponse.items?.count).to(equal(events?.count))
+                            
+                            for res in ddbResponse.items! {
+                                expect(res["event_type"]?.s).to(equal(events![0].eventType))
+                                
+                                let deleteInput = AWSDynamoDBDeleteItemInput()
+                                deleteInput?.tableName = "events_\(clientId)"
+                                deleteInput?.key = ["user_id": keyVal!, "timestamp": res["timestamp"]!]
+                                dynamodb.deleteItem(deleteInput!) {
+                                    (res, err) in if let error = err {
+                                        print(error.localizedDescription)
+                                        fail()
+                                    }
+                                }
+                                
+                                completionHandler(true, nil)
+                            }
+                        } else {
+                            fail()
+                        }
                     }
                 }
+            } else {
+                fail()
             }
-        } else {
-            fail()
         }
+    } catch let error {
+        fail()
+        print(error)
     }
 }
 
@@ -137,61 +140,68 @@ public func checkSuccessfulEventUpdate(event: BaseEvent?, newEvent: BaseEvent?, 
     condition?.attributeValueList = [keyVal!]
     let expression = ["user_id": condition!]
     queryInput?.keyConditions = expression
+    
+    do {
+        let zaiClient = try ZaiClient(zaiClientID: clientId, zaiSecret: clientSecret)
 
-    zaiClient.addEventLog(event!) {
-        (res, error) in if let response = res {
-            
-            expect(response.failureCount).to(equal(0))
-            
-            if events?.count == 1 {
-                let e = events![0]
-                dynamodb.query(queryInput!) {
-                    (ddbRes, ddbError) in if let ddbResponse = ddbRes {
-                        expect(ddbResponse.items![0]["item_id"]?.s).to(equal(e.itemId))
-                        expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
-                        expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
-                        
-                        do {
-                            try zaiClient.updateEventLog(newEvent!) {
-                                (res, error) in if let response = res {
-                                    expect(response.failureCount).to(equal(0))
-                                    
-                                    let e = newEvents![0]
-                                    dynamodb.query(queryInput!) {
-                                        (ddbRes, ddbError) in if let ddbResponse = ddbRes {
-                                            expect(ddbResponse.items![0]["item_id"]?.s).to(equal(e.itemId))
-                                            expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
-                                            expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
-                                            
-                                            let deleteInput = AWSDynamoDBDeleteItemInput()
-                                            deleteInput?.tableName = "events_\(clientId)"
-                                            deleteInput?.key = ["user_id": keyVal!, "timestamp": ddbResponse.items![0]["timestamp"]!]
-                                            dynamodb.deleteItem(deleteInput!) {
-                                                (res, err) in if let error = err {
-                                                    print(error.localizedDescription)
-                                                    fail()
+        zaiClient.addEventLog(event!) {
+            (res, error) in if let response = res {
+                
+                expect(response.failureCount).to(equal(0))
+                
+                if events?.count == 1 {
+                    let e = events![0]
+                    dynamodb.query(queryInput!) {
+                        (ddbRes, ddbError) in if let ddbResponse = ddbRes {
+                            expect(ddbResponse.items![0]["item_id"]?.s).to(equal(e.itemId))
+                            expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
+                            expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
+                            
+                            do {
+                                try zaiClient.updateEventLog(newEvent!) {
+                                    (res, error) in if let response = res {
+                                        expect(response.failureCount).to(equal(0))
+                                        
+                                        let e = newEvents![0]
+                                        dynamodb.query(queryInput!) {
+                                            (ddbRes, ddbError) in if let ddbResponse = ddbRes {
+                                                expect(ddbResponse.items![0]["item_id"]?.s).to(equal(e.itemId))
+                                                expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
+                                                expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
+                                                
+                                                let deleteInput = AWSDynamoDBDeleteItemInput()
+                                                deleteInput?.tableName = "events_\(clientId)"
+                                                deleteInput?.key = ["user_id": keyVal!, "timestamp": ddbResponse.items![0]["timestamp"]!]
+                                                dynamodb.deleteItem(deleteInput!) {
+                                                    (res, err) in if let error = err {
+                                                        print(error.localizedDescription)
+                                                        fail()
+                                                    }
                                                 }
+                                                
+                                                completionHandler(true, nil)
+                                            } else {
+                                                fail()
                                             }
-                                            
-                                            completionHandler(true, nil)
-                                        } else {
-                                            fail()
                                         }
                                     }
                                 }
+                            } catch {
+                                fail()
                             }
-                        } catch {
+                        } else {
                             fail()
                         }
-                    } else {
-                        fail()
                     }
+                    
                 }
-                
+            } else {
+                fail()
             }
-        } else {
-            fail()
         }
+    } catch let error {
+        fail()
+        print(error)
     }
 }
 
@@ -211,43 +221,50 @@ public func checkSuccessfulEventDelete(event: BaseEvent?, completionHandler: @es
     condition?.attributeValueList = [keyVal!]
     let expression = ["user_id": condition!]
     queryInput?.keyConditions = expression
+    
+    do {
+        let zaiClient = try ZaiClient(zaiClientID: clientId, zaiSecret: clientSecret)
 
-    zaiClient.addEventLog(event!) {
-        (res, error) in if let response = res {
-            
-            expect(response.failureCount).to(equal(0))
-            
-            if events?.count == 1 {
-                let e = events![0]
-                dynamodb.query(queryInput!) {
-                    (ddbRes, ddbError) in if let ddbResponse = ddbRes {
-                        expect(ddbResponse.items![0]["item_id"]?.s).to(equal(e.itemId))
-                        expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
-                        expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
-                        
-                        zaiClient.deleteEventLog(event!) {
-                            (res, error) in if let response = res {
-                                expect(response.failureCount).to(equal(0))
-                                
-                                dynamodb.query(queryInput!) {
-                                    (ddbRes, ddbError) in if let ddbResponse = ddbRes {
-                                        expect(ddbResponse.items?.count).to(equal(0))
-                                        completionHandler(true, nil)
-                                    } else {
-                                        fail()
+        zaiClient.addEventLog(event!) {
+            (res, error) in if let response = res {
+                
+                expect(response.failureCount).to(equal(0))
+                
+                if events?.count == 1 {
+                    let e = events![0]
+                    dynamodb.query(queryInput!) {
+                        (ddbRes, ddbError) in if let ddbResponse = ddbRes {
+                            expect(ddbResponse.items![0]["item_id"]?.s).to(equal(e.itemId))
+                            expect(ddbResponse.items![0]["event_value"]?.s).to(equal(e.eventValue))
+                            expect(ddbResponse.items![0]["event_type"]?.s).to(equal(e.eventType))
+                            
+                            zaiClient.deleteEventLog(event!) {
+                                (res, error) in if let response = res {
+                                    expect(response.failureCount).to(equal(0))
+                                    
+                                    dynamodb.query(queryInput!) {
+                                        (ddbRes, ddbError) in if let ddbResponse = ddbRes {
+                                            expect(ddbResponse.items?.count).to(equal(0))
+                                            completionHandler(true, nil)
+                                        } else {
+                                            fail()
+                                        }
                                     }
                                 }
                             }
+                        } else {
+                            fail()
                         }
-                    } else {
-                        fail()
                     }
+                    
                 }
-                
+            } else {
+                fail()
             }
-        } else {
-            fail()
         }
+    } catch let error {
+        print(error)
+        fail()
     }
 }
 
@@ -262,33 +279,53 @@ func convertToDictionary(_ text: String?) -> [String: String]? {
     return nil
 }
 
+struct Metadata: Decodable {
+    var userId: String?
+    var itemId: String?
+    var itemIds: [String]?
+    var limit: Int
+    var offset: Int
+    var options: [String: String]?
+    var recommendationType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case itemId = "item_id"
+        case itemIds = "item_ids"
+        case limit = "limit"
+        case offset = "offset"
+        case options = "options"
+        case recommendationType = "recommendation_type"
+    }
+}
+
 public func checkSuccessfulRecommendation(recommendation: RecommendationRequest?, completionHandler: @escaping (_ response: Bool?,_ error: Error?) -> ()) {
-    zaiClient.getRecommendations(recommendation!) {
-        (res, err) in if let response = res {
-            let items = response.items
-            expect(response.count).to(equal(recommendation?.limit))
-            expect(response.items.count).to(equal(recommendation?.limit))
-            
-            if recommendation?.options != nil {
-                let recommendationOptions = convertToDictionary(recommendation?.options)
+    do {
+        let zaiClient = try ZaiClient(zaiClientID: clientId, zaiSecret: clientSecret)
+
+        zaiClient.getRecommendations(recommendation!) {
+            (res, err) in if let response = res {
+                let metadata = try? JSONDecoder().decode(Metadata.self, from: response.metadata.data(using: .utf8)!)
+                expect(response.count).to(equal(recommendation?.limit))
+                expect(response.items.count).to(equal(recommendation?.limit))
                 
-                let optionStrArr = recommendationOptions!.map {
-                    (k, v) in return "\(k):\(v)"
+                expect((metadata?.recommendationType)).to(equal(recommendation?.recommendationType))
+                expect((metadata?.offset)).to(equal(recommendation?.offset))
+                if let options = recommendation?.options {
+                    let _options = try! JSONSerialization.jsonObject(with: Data(options.utf8), options: []) as? [String: Any]
+                    let metadataOptions = metadata?.options
+                    expect(metadataOptions! as NSDictionary).to(equal(_options as! NSDictionary))
+                } else {
+                    expect(recommendation?.options).to(beNil())
                 }
                 
-                let optionsLength = optionStrArr.count
-                
-                let options = Array(items[0].split(separator: "|")[2..<2+optionsLength])
-                expect(options.count).to(equal(optionStrArr.count))
-                
+                completionHandler(true, nil)
+            } else {
+                fail()
             }
-            
-            let pageType = String(items[0].split(separator: "|")[1])
-            expect(pageType).to(equal(recommendation?.recommendationType))
-            
-            completionHandler(true, nil)
-        } else {
-            fail()
         }
+    } catch let error {
+        print(error)
+        fail()
     }
 }
