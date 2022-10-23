a2c0e4c57139fbb146fe28276b1c45dd97117cc9
kyungwon-lee <kw375310@gmail.com>
2022-10-23 18:49:38 +0900

    add warning message when decoding metadata fails

--- Sources/zaiclient/Classes/Response/RecommendationResponse.swift
+++ Sources/zaiclient/Classes/Response/RecommendationResponse.swift
@@ -9,4 +9,20 @@ public struct RecommendationResponse: Decodable {
     enum CodingKeys: String, CodingKey {
         case items, count, metadata, timestamp
     }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        items = try container.decode([String].self, forKey: .items)
+        count = try container.decode(Int.self, forKey: .count)
+        metadata = try container.decode(String.self, forKey: .metadata)
+        timestamp = try container.decode(Double.self, forKey: .timestamp)
+        
+        do {
+            try JSONSerialization.jsonObject(with: Data(container.decode(String.self, forKey: .metadata).utf8), options: .mutableContainers)
+        } catch {
+            print("WARNING: Failed to parse the metadata to object, returning an empty object. metadata: \(metadata)")
+            metadata = "{}"
+        }
+
+    }
 }
